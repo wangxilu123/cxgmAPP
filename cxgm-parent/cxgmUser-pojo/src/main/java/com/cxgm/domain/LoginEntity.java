0e038c927620ec6c716d0aff960bd54d4031ff67
lulu <lulu@lulu-PC.lan>
2018-05-05 14:58:10 +0800

    修改问题

--- cxgm-parent/cxgmUser-pojo/src/main/java/com/cxgm/domain/LoginEntity.java
+++ cxgm-parent/cxgmUser-pojo/src/main/java/com/cxgm/domain/LoginEntity.java
@@ -16,4 +16,22 @@ public class LoginEntity implements Serializable{
     
     @ApiModelProperty(name = "mobileValidCode",value = "验证码")
     private String mobileValidCode;
+
+	public String getUserAccount() {
+		return userAccount;
+	}
+
+	public void setUserAccount(String userAccount) {
+		this.userAccount = userAccount;
+	}
+
+	public String getMobileValidCode() {
+		return mobileValidCode;
+	}
+
+	public void setMobileValidCode(String mobileValidCode) {
+		this.mobileValidCode = mobileValidCode;
+	}
+    
+    
 }
