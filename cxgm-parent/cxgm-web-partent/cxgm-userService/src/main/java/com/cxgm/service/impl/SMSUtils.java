a0e323c80e67f31475a538bf1dc13dc0c6782560
lulu <lulu@lulu-PC.lan>
2018-05-05 15:15:12 +0800

    修改问题

---
+++ cxgm-parent/cxgm-web-partent/cxgm-userService/src/main/java/com/cxgm/service/impl/SMSUtils.java
@@ -0,0 +1,70 @@
+package com.cxgm.service.impl;
+
+import com.aliyuncs.DefaultAcsClient;
+import com.aliyuncs.IAcsClient;
+import com.aliyuncs.dysmsapi.model.v20170525.SendSmsRequest;
+import com.aliyuncs.dysmsapi.model.v20170525.SendSmsResponse;
+import com.aliyuncs.exceptions.ClientException;
+import com.aliyuncs.profile.DefaultProfile;
+import com.aliyuncs.profile.IClientProfile;
+
+/**
+ * @Description:短信验证工具类
+ */
+public class SMSUtils {
+
+	 //产品名称:云通信短信API产品,开发者无需替换
+    static final String product = "Dysmsapi";
+    //产品域名,开发者无需替换
+    static final String domain = "dysmsapi.aliyuncs.com";
+
+    // TODO 此处需要替换成开发者自己的AK(在阿里云访问控制台寻找)
+    static final String accessKeyId = "LTAItzO0XhCesKeP";
+    static final String accessKeySecret = "cUu5VqFEZW2oL5pEihMt64cVUTXg4z";
+      
+    public String  sendSms(String phoneName) throws ClientException {
+
+        //可自助调整超时时间
+        System.setProperty("sun.net.client.defaultConnectTimeout", "10000");
+        System.setProperty("sun.net.client.defaultReadTimeout", "10000");
+
+        //初始化acsClient,暂不支持region化
+        IClientProfile profile = DefaultProfile.getProfile("cn-hangzhou", accessKeyId, accessKeySecret);
+        DefaultProfile.addEndpoint("cn-hangzhou", "cn-hangzhou", product, domain);
+        IAcsClient acsClient = new DefaultAcsClient(profile);
+
+        //组装请求对象-具体描述见控制台-文档部分内容
+        SendSmsRequest request = new SendSmsRequest();
+        //必填:待发送手机号
+        request.setPhoneNumbers(phoneName);
+        //必填:短信签名-可在短信控制台中找到
+        request.setSignName("阿里云短信测试专用");
+        //必填:短信模板-可在短信控制台中找到
+        request.setTemplateCode("SMS_134145386");
+        //可选:模板中的变量替换JSON串,如模板内容为"亲爱的${name},您的验证码为${code}"时,此处的值为
+        
+        int code = getCode(1,999999);
+        request.setTemplateParam("{\"code\":\""+code+"\"}");
+
+        //选填-上行短信扩展码(无特殊需求用户请忽略此字段)
+        //request.setSmsUpExtendCode("90997");
+
+        //可选:outId为提供给业务方扩展字段,最终在短信回执消息中将此值带回给调用者
+        request.setOutId("yourOutId");
+
+        //hint 此处可能会抛出异常，注意catch
+        SendSmsResponse sendSmsResponse = acsClient.getAcsResponse(request);
+        
+        return String.valueOf(code);
+    }  
+      
+        
+    /** 
+     * 随机生成6位数字作为验证码 
+     * @return 
+     */  
+    public static int getCode(int min, int max){  
+    	int randNum = min + (int)(Math.random() * ((max - min) + 1));
+        return randNum;
+    }  
+}
